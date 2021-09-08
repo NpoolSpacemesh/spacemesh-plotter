aaa69efaf132c3dae736a84393966abf2934e150
Zhao KK <kikakkz@hotmail.com>
2021-09-08 16:59:44 +0800

    Add plot application code

---
+++ plot.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spacemeshos/post/config"
+	"github.com/spacemeshos/post/initialization"
+)
+
+func main() {
+	providers := initialization.Providers()
+	for i, provider := range providers {
+		fmt.Printf("PROVIDER %v: %v\n", i, provider.Model)
+	}
+
+	fmt.Printf("Plot file with %v\n", providers[0].Model)
+	init, err := initialization.NewInitializer(
+		config.Config{
+			BitsPerLabel:  32,
+			LabelsPerUnit: 32,
+			MaxNumUnits:   4096 * 1024 * 32,
+			MinNumUnits:   16,
+		},
+		config.InitOpts{
+			NumFiles:          1,
+			NumUnits:          4096 * 1024 * 32,
+			DataDir:           "./plot",
+			ComputeProviderID: int(providers[0].ID),
+		},
+		make([]byte, 32),
+	)
+	if err != nil {
+		fmt.Printf("fail to create new initializer: %v\n", err)
+		return
+	}
+
+	err = init.Initialize()
+	if err != nil {
+		fmt.Printf("fail to initialize file: %v\n", err)
+		return
+	}
+}
